d03efda31a4d46ccde2ef0e9dede21ac18147e7c
Lauren Smith <laurenkimsmith@gmail.com>
2025-08-31 14:25:57 +0100

    Added some info to my loops file about iterables.

---
+++ Loops.py
@@ -0,0 +1,7 @@
+"""
+Iterables - when thinking about whether something is an iterable,
+ask yourself the question, "Does it hold more than 1 data type?
+If yes, it's an iterable. Dictionaries and lists
+are examples of iterables. Strings are also considered
+iterables, because Python views strings as lists of characters.
+"""
